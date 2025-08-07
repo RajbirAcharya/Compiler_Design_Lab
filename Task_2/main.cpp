45aa92dab1748bba2677e3fd2e29f400e95f4414
RajbirAcharya <106105644+RajbirAcharya@users.noreply.github.com>
2025-08-07 10:31:35 +0600

    Lab Task 3_2

--- Task_2/main.cpp
+++ Task_2/main.cpp
@@ -1,26 +1,73 @@
-//22-48080-2 12:34 PM
+//22-48080-2
 
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
+bool isDigit(char ch) {
+    return (ch >= '0' && ch <= '9');
+}
+
+bool isDelimiter(char ch) {
+    char delimiters[] = {';', ',', '(', ')', '{', '}', '[', ']', ' ', '\n', '\t'};
+    for (int i = 0; i < sizeof(delimiters); ++i) {
+        if (ch == delimiters[i]) return true;
+    }
+    return false;
+}
+
 int main() {
-    char input[100];
-    cout << "Enter the equation: ";
-    cin >> input;
+    ifstream file("program.txt");
 
-    int operatorCount = 0;
+    if (!file) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
 
-    for (int i = 0; input[i] != '\0'; i++) {
-        char ch = input[i];
-        if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == '=') {
-            operatorCount++;
-            cout << "operator" << operatorCount << ": " << ch << endl;
+    char ch;
+    string input = "";
+
+    while (file.get(ch)) {
+        if ((ch >= '0' && ch <= '9') || ch == '.' || ch == '-') {
+            input += ch;
+        } else if (isDelimiter(ch)) {
+            if (input != "") {
+                int dotCount = 0;
+                bool validNumber = true;
+
+                for (int i = 0; i < input.length(); i++) {
+                    if (input[i] == '.') {
+                        dotCount++;
+                        if (dotCount > 1) {
+                            validNumber = false;
+                            break;
+                        }
+                    } else if (!(input[i] == '-' && i == 0) && !(input[i] >= '0' && input[i] <= '9')) {
+                        validNumber = false;
+                        break;
+                    }
+                }
+
+                if (validNumber) {
+                    if (dotCount == 0) {
+                        cout << "Integer: " << input << endl;
+                    } else {
+                        cout << "Real Number: " << input << endl;
+                    }
+                }
+
+                input = "";
+            }
+
+            if (ch != ' ' && ch != '\n' && ch != '\t') {
+                cout << "Delimiter: " << ch << endl;
+            }
+        } else {
+            if (input != "") input = "";
         }
     }
 
-    if (operatorCount == 0) {
-        cout << "No operators found." << endl;
-    }
-
+    file.close();
     return 0;
 }
